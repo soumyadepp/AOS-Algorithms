df5bc297bff1bd5228448b4bdc619dcf70c24f4e
Soumyadeep Ghosh <67363979+soumyadepp@users.noreply.github.com>
2022-03-09 16:00:58 +0530

    Update Bankers.cpp

--- BankersAlgorithm/Bankers.cpp
+++ BankersAlgorithm/Bankers.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//check if all the processes are finished
 bool allTrue(vector<bool>&finished){
     for(auto i : finished){
         if(i == false) return false;
     }
     return true;
 }
+//function to implement bankers algorithm
 string bankersAlgorithm(vector<vector<int>>&allocated,vector<vector<int>>&maxNeed,vector<int>&resources){
     int r = resources.size();
     int n = allocated.size();
@@ -16,14 +18,17 @@ string bankersAlgorithm(vector<vector<int>>&allocated,vector<vector<int>>&maxNee
               columnSum[i] += allocated[j][i];
         }
     }
+    //calculate the number of each resource that is available
     vector<int>available(r);
     for(int i = 0 ; i < r ; i++){
         available[i] = resources[i] - columnSum[i];
+        //if the availability of a resource is negative, it means its not enough
         if(available[i] < 0){
             return "Not enough resources\n";
         }
     }
     string ans = "";
+    //now we will calculate need
     vector<vector<int>>need;
     for(int i = 0 ; i < n ; i++){
         vector<int>temp(r);
@@ -32,8 +37,10 @@ string bankersAlgorithm(vector<vector<int>>&allocated,vector<vector<int>>&maxNee
         }
         need.push_back(temp);
     }
+    //to keep track of finished processes
     vector<bool>finished(r,false);
     string res = "";
+    //till all processes are not finished or deadlock is detected
     while(allTrue(finished) == false){
         int numberOfProcessesFinishedInPass = 0;
         for(int i = 0 ; i < n ; i++){
@@ -50,18 +57,20 @@ string bankersAlgorithm(vector<vector<int>>&allocated,vector<vector<int>>&maxNee
                     for(int j = 0 ; j < allocated[i].size(); j++){
                         available[j] += allocated[i][j];
                     }
-                    res += "P" + to_string(i+1) + " ";    
+                    res += "P" + to_string(i) + " ";    
                     numberOfProcessesFinishedInPass++;         
                 }
             }
         }
+        //if one pass is over and we have not serviced atleast 1 process,
+        //the system is in deadlock
         if(numberOfProcessesFinishedInPass == 0) return "Deadlock detected\n";
     }
     return res;
 }
 
 int main(){
-    vector<vector<int>>allocated = {{0,1,0},{2,0,0},{3,0,2},{2,1,1},{0,0,2}};
+    vector<vector<int>>allocated = {{0,1,0},{4,0,0},{3,0,2},{2,1,1},{0,0,2}};
     vector<vector<int>>maxNeed = {{7,2,3},{3,2,2},{9,0,2},{2,2,2},{4,3,3}};
     vector<int>resources = {10,5,7};
     string result = bankersAlgorithm(allocated,maxNeed,resources);
